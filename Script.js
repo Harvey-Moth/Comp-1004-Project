5f2983bae790e113c9cf7855bf2b74ddfd1493d7
Harvey Moth <Harvey.moth@students.plymouth.ac.uk>
2024-04-08 16:01:00 +0100

    Adapted the nav bar and started implementing a json storage system
    Changed the navigation bar to the correct titling and started implementing the json storage, will add more functionality like adding data in later commit

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
-document.getElementById('toploginbutton').addEventListener('click',(event) => {
+document.getElementById('submitnewpass').addEventListener('click',(event) => {
   
-    var lvis = document.getElementById("Logincontent");
+    var lvis = document.getElementById("newpass");
      
       lvis.style.display = "block";
       var hvis = document.getElementById("Homecontent");
@@ -11,7 +11,20 @@ document.getElementById('tophomebutton').addEventListener('click',(event) => {
   
   var hvis = document.getElementById("Homecontent");
     hvis.style.display = "block";
-  var lvis = document.getElementById("Logincontent");
+  var lvis = document.getElementById("newpass");
      
       lvis.style.display = "none";
 })
+
+function Passwordvis(Passwordvisibility) {
+  let Passvis = document.getElementById(Passwordvisibility)
+  if (Passvis.type === "password")
+  {
+    Passvis.type = "text";
+  }
+  else
+  {
+    Passvis.type = "password";
+  }
+
+}
