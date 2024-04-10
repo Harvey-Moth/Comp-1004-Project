e2bbb632ba7c5ade712866d7149a15e0c274fe99
Harvey Moth <Harvey.moth@students.plymouth.ac.uk>
2024-04-10 14:41:59 +0100

    Added a new page and developed the local storage more
    Added a new page and developed the JS for the local storage system more.

--- Script.js
+++ Script.js
@@ -1,19 +1,28 @@
+var hvis = document.getElementById("Homecontent");
+var lvis = document.getElementById("newpass");
+var avis = document.getElementById("accessibilitysettings");
 document.getElementById('submitnewpass').addEventListener('click',(event) => {
   
-    var lvis = document.getElementById("newpass");
-     
+    
+    
       lvis.style.display = "block";
-      var hvis = document.getElementById("Homecontent");
       hvis.style.display = "none";
-
+      avis.style.display = "none";
+      
 } )
 document.getElementById('tophomebutton').addEventListener('click',(event) => {
   
-  var hvis = document.getElementById("Homecontent");
-    hvis.style.display = "block";
-  var lvis = document.getElementById("newpass");
-     
-      lvis.style.display = "none";
+  
+  hvis.style.display = "block";
+  avis.style.display = "none";
+  lvis.style.display = "none";
+})     
+
+document.getElementById('accessibility').addEventListener('click',(event) => {
+  
+  avis.style.display = "block";   
+  lvis.style.display = "none";
+  hvis.style.display = "none";
 })
 
 function Passwordvis(Passwordvisibility) {
@@ -54,14 +63,17 @@ function Registeruser() {
   var usernameinput1 = document.getElementById("Uname").value;
   var usernameinput2 = document.getElementById("Unamecheck").value;  
   var passwordinput = document.getElementById("Mpass").value;
-
+  var Exsistingusername = false
   if(usernameinput1 === usernameinput2){
 
   for(let i = 0; i < JFdata.Users.length; i++) { 
-    if(JFdata.Users[i].Username===usernameinput1)
+    if(JFdata.Users[i].Username===usernameinput1) {
     alert("This user already exits"); 
     console.log(JFdata.users[i]);
+    Exsistingusername = true
+    }
   }  
+  
 
 
 
